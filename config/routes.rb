097cd360a39f0a3a4ceb044bc9c007a0539eb16f
Clinton Enyinna <enyinnaclinton@gmail.com>
2020-03-05 15:09:25 -0600

    add login, logout, signup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+  
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  get '/signup', to: 'users#new'
   
   resources :users, only: [:new, :create, :show]
 end
