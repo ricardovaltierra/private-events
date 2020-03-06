6e17eb317baaf01266218852d51ca52e08502748
Clinton Enyinna <enyinnaclinton@gmail.com>
2020-03-06 13:50:45 -0600

    refactor event views and add dashboard link to user's profile

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -18,7 +18,7 @@ class EventsController < ApplicationController
   end
 
   def show
-    @event = current_user.created_events.find(params[:id])
+    @event = Event.find(params[:id])
   end
 
   private
