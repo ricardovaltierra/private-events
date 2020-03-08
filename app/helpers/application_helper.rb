24d46ba7689a009a9163573e8ab4a0786f538720
Clinton Enyinna <enyinnaclinton@gmail.com>
2020-03-08 03:42:15 -0600

    add necessary helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,13 @@
 module ApplicationHelper
+    attr_accessor :registered
+
     def current_user
       if (user_id = session[:user_id])
         @current_user ||= User.find_by(id: user_id)
       end
-    end    
+    end
 
+    def registered?(user)
+      @registered ||= @event.attendees.exists?(user.id)
+    end
 end
