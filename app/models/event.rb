c3c0f07371a9f03750da10178cc8c0c6dccf1392
ricardovaltierra <ricardo_valtierra@outlook.com>
2020-03-09 14:21:23 -0600

    Class methods and scopes  on Event model to split upcoming / past

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,16 @@
 class Event < ApplicationRecord  
   belongs_to :creator, class_name: "User"
   has_and_belongs_to_many :attendees, class_name: "User"
+
+  # def self.past    
+  #   Event.all.where("date < ?", Time.now)    
+  # end
+
+  scope :past, -> { where("date < ?", Time.now) }
+
+  # def self.upcoming
+  #   Event.all.where("date > ?", Time.now)
+  # end
+
+  scope :upcoming, -> { where("date > ?", Time.now) }
 end
